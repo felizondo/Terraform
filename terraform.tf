6603d41e63ea61650be99d8437b3890d4877faa9
felizondo <68337720+felizondo@users.noreply.github.com>
2024-12-19 17:40:14 -0600

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.97"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.48"
+    }
+       databricks = {
+      source  = "databricks/databricks"
+      version = "1.42.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name   = "AKS"
+    storage_account_name  = "felizondotsstorage"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}

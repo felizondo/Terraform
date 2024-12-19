372681de3c93b3fcf0ca806ca859ae9b0b3679b5
felizondo <68337720+felizondo@users.noreply.github.com>
2024-12-19 17:45:39 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,54 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
-    }
-  }
-
-  # Update this block with the location of your terraform state file
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
-  }
-}
-
 provider "azurerm" {
-  features {}
-  use_oidc = true
+  features {}  
+  skip_provider_registration = true
 }
 
-# Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-resource "azurerm_resource_group" "rg-aks" {
-  name     = var.resource_group_name
-  location = var.location
+
+module "networking" {
+  source              = "./modules/networking"
+  groupname           = var.group_name
+  location            = var.locations
 }
+
+module "storage" {
+  source = "./modules/storage"
+  resource_group_name = var.group_name
+  location = var.locations
+}
+
+module "Databric" {
+  source              = "./modules/databricks"
+  resource_group_name = var.group_name
+  location            = var.locations
+  vnet                = module.networking.my_vnet
+  subnet              = module.networking.name_subnet
+  subnet_private      = module.networking.name_subneprivate
+  nsg_mysubnet        = module.networking.nsg_mysubnet
+  nsg_mysubneprivate  = module.networking.nsg_mysubneprivate
+}
+
+/*
+module "VM1" {
+  source               = "./modules/vm"
+  name_vm              = "VMCVH1"
+  nameinterface_vm     = "InterfaceVM1"
+  resource_group_name  = module.networking.group_name
+  location             = var.locations
+  admin_username       = "adminuser"
+  ssh_public_key       = "ssh-rsa ..."
+  public_ip            = "myPublicIP1"
+  subnet_id            = module.networking.my_subnet 
+}
+
+module "VM2" {
+  source               = "./modules/vm"
+  name_vm              = "VMCVH2"
+  nameinterface_vm     = "InterfaceVM2"
+  resource_group_name  = module.networking.group_name
+  location             = var.locations
+  admin_username       = "adminuser"
+  ssh_public_key       = "ssh-rsa ..."
+  public_ip            = "myPublicIP2"
+  subnet_id            = module.networking.my_subnet 
+}
+*/

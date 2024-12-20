edc3464198dfca64a0c247d67b43a8178f237b41
felizondo <68337720+felizondo@users.noreply.github.com>
2024-12-19 18:56:34 -0600

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "storagedatabricks"
+  name                     = "felizondodatabricks"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
